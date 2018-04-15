87c3c3335275f3f2d01549e133607a3327cb8a9a
Andhitia Rama <andhitia.r@gmail.com>
2018-04-15 08:35:08 +0700

    [8.0.1.0.0] accountant_app

---
+++ accountant_app/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Accountant Service App",
+    "summary": "Manage Accountancy Service",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "wizards/convert_product_to_service.xml",
+        "views/accountant_config_setting_views.xml",
+        "views/accountant_service_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
