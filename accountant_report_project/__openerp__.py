cdea2c508ce090e6969080dab555e163ad1d2b7d
Andhitia Rama <andhitia.r@gmail.com>
2018-04-16 16:32:21 +0700

    [8.0.1.0.0] accountant_report_project

---
+++ accountant_report_project/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Accountant Report to Project Link",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_report",
+        "project",
+    ],
+    "data": [
+        "views/accountant_report_views.xml",
+    ],
+    "installable": True,
+}
