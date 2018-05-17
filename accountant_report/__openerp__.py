b03b498fe6ef333338d94690c54b08505ccf27ec
Andhitia Rama <andhitia.r@gmail.com>
2018-05-17 10:02:51 +0700

    [8.0.1.2.0] accountant_report]
    * Fix sequence domain
    * Fix related field on config

--- accountant_report/__openerp__.py
+++ accountant_report/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Manage Accountant Report",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "license": "AGPL-3",
