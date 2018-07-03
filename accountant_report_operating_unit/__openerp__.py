3ec2ef5e118f29ef69cbaee4d58fa5eac30b5739
Andhitia Rama <andhitia.r@gmail.com>
2018-07-03 23:02:01 +0700

    [8.0.1.0.0] account_report_operating_unit

---
+++ accountant_report_operating_unit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Operating Unit for Accountant Report",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_report",
+        "operating_unit",
+    ],
+    "data": [
+        "security/ir_rule_data.xml",
+        "views/accountant_report_views.xml",
+    ],
+    "installable": True,
+}
---
+++ accountant_report_operating_unit/models/accountant_report.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class AccountantReport(models.Model):
+    _inherit = "accountant.report"
+
+    @api.model
+    def _default_operating_unit_id(self):
+        return self.env["res.users"].operating_unit_default_get(self._uid)
+
+    operating_unit_id = fields.Many2one(
+        string="Operating Unit",
+        comodel_name="operating.unit",
+        default=lambda self: self._default_operating_unit_id(),
+    )
