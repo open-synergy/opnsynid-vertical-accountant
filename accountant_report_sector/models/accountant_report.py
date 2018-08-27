c698e8ba5ca195836e96e5fb5b2452c6d6d3ea94
Andhitia Rama <andhitia.r@gmail.com>
2018-08-27 17:12:58 +0700

    [8.0.1.0.0] accountant_report_sector

---
+++ accountant_report_sector/models/accountant_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class AccountantReport(models.Model):
+    _inherit = "accountant.report"
+
+    primary_sector_id = fields.Many2one(
+        string="Primary Sector",
+        comodel_name="res.partner.sector",
+        related="partner_id.sector_id",
+        store=True,
+        readonly=True,
+    )
