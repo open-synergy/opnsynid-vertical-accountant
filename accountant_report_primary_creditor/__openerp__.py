3ebe34106ae4a21f122126ac02eebf447778c2a4
Andhitia Rama <andhitia.r@gmail.com>
2018-08-27 07:22:26 +0700

    [8.0.1.0.0] accountant_report_primary_creditor

---
+++ accountant_report_primary_creditor/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Creditor Information on Accountant Report",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_report",
+        "partner_creditor_debitor",
+    ],
+    "data": [
+        "views/accountant_report_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
