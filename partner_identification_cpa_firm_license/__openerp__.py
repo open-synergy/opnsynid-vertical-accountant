bb332c9b523d52d7545ac12d485c0eda344c127a
Andhitia Rama <andhitia.r@gmail.com>
2018-09-01 02:37:28 +0700

    [8.0.1.0.0] partner_identification_cpa_firm_license

---
+++ partner_identification_cpa_firm_license/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Identification - CPA Firm License",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+            "partner_identification",
+    ],
+    "external_dependencies": {
+        "python": [
+            "openupgradelib",
+        ],
+    },
+    "data": [
+        "data/res_partner_id_category_data.xml",
+    ],
+    "installable": True,
+    "pre_init_hook": "_load_partner_identification_data",
+}
---
+++ partner_identification_cpa_firm_license/hooks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def _load_partner_identification_data(cr):
+    try:
+        from openupgradelib import load_data
+        load_data(
+            cr, "partner_identification_cpa_firm_license",
+            "/data/res_partner_id_category_data.xml")
+    except:
+        return
