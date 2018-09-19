0efdf6b20c2ebd682ebbe51fd530f7a01d88fce1
Andhitia Rama <andhitia.r@gmail.com>
2018-09-19 14:12:53 +0700

    [8.0.1.0.0] accountant_auditor

---
+++ accountant_auditor/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Contact - Auditor",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+            "base",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
