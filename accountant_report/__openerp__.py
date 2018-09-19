33c6daa2ae82dd3920614e2ae8def0e4ea121669
Andhitia Rama <andhitia.r@gmail.com>
2018-09-19 22:08:03 +0700

    [8.0.1.4.0] accountant_report

--- accountant_report/__openerp__.py
+++ accountant_report/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Manage Accountant Report",
-    "version": "8.0.1.3.1",
+    "version": "8.0.1.4.0",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "license": "AGPL-3",
@@ -13,6 +13,7 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "security/ir_module_category_data.xml",
         "data/ir_sequence_data.xml",
         "views/accountant_config_setting_views.xml",
         "views/accountant_report_opinion_views.xml",
