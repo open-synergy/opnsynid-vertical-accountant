f2f8a0095d9df85f96dd95dc08a50a49074899f0
Andhitia Rama <andhitia.r@gmail.com>
2018-09-20 12:48:12 +0700

    [8.0.1.0.0] accountant_project

---
+++ accountant_project/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Accountant Service to Project Link",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_app",
+        "project",
+    ],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+    "installable": True,
+}
---
+++ accountant_project/models/project_project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    accountant_service_id = fields.Many2one(
+        string="Accountant Service",
+        comodel_name="accountant.service",
+    )
