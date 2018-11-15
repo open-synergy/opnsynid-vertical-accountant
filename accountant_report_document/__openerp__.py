256b7f23ff66390d26fafd4a391140dc36281dcd
Andhitia Rama <andhitia.r@gmail.com>
2018-11-15 11:01:38 +0700

    [8.0.1.1.0] accountant_report_document

---
+++ accountant_report_document/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Integration Between Accountant Report and Document",
+    "version": "8.0.1.1.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_report",
+        "document",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
