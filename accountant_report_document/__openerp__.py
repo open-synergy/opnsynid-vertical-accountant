25fa84f3d2f4b2898318d3e84852427bec68b8c9
Andhitia Rama <andhitia.r@gmail.com>
2019-12-03 16:31:25 +0700

    [8.0.1.2.0] accountant_report_document
    * Add main report attachment field

--- accountant_report_document/__openerp__.py
+++ accountant_report_document/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Integration Between Accountant Report and Document",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "license": "AGPL-3",
@@ -13,6 +13,7 @@
         "document",
     ],
     "data": [
+        "views/accountant_report_views.xml",
     ],
     "installable": True,
 }
