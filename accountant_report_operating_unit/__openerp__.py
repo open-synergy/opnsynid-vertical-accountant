acee91d2ad82963647c4d9f68bd91d08a7d1297f
Andhitia Rama <andhitia.r@gmail.com>
2020-09-19 08:25:23 +0700

    [accountant_report_operating_unit] 8.0.1.3.0
    * Fix view inheritance

--- accountant_report_operating_unit/__openerp__.py
+++ accountant_report_operating_unit/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Operating Unit for Accountant Report",
-    "version": "8.0.1.1.0",
-    "author": "OpenSynergy Indonesia",
-    "website": "https://opensynergy-indonesia.com",
+    "version": "8.0.1.3.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
     "license": "AGPL-3",
     "depends": [
         "accountant_report",
