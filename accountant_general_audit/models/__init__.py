703f9f35b670b69cadf9d50c3f3022ab9d346ba1
Andhitia Rama <andhitia.r@gmail.com>
2021-04-07 14:41:12 +0700

    [accountant_general_audit] 8.0.1.0.0

---
+++ accountant_general_audit/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    accountant_client_account_type_set,
+    accountant_client_account_type,
+    accountant_client_trial_balance_detail,
+    accountant_client_trial_balance,
+    tier_definition,
+)
---
+++ accountant_general_audit/models/accountant_client_account_type.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class AccountantClientAccountType(models.Model):
+    _name = "accountant.client_account_type"
+    _description = "Accountant Client Account Type"
+    _order = "sequence, id"
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+        required=True,
+    )
+    sequence = fields.Integer(
+        string="Sequence",
+        required=True,
+        default=5,
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    description = fields.Text(
+        string="Description",
+    )
