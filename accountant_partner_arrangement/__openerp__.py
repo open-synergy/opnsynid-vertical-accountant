ed4a0e8b449e597c497f26c9b3efd35a9505a56c
azmimr67@gmail.com <azmimr67@gmail.com>
2021-06-01 16:45:53 +0700

    [accountant_partner_arrangement] 8.0.2.0.0
    Fix #71, #72, #73, #74, #75

--- accountant_partner_arrangement/__openerp__.py
+++ accountant_partner_arrangement/__openerp__.py
@@ -4,16 +4,24 @@
 
 {
     "name": "Accountant Firm Partner Arrangement",
-    "version": "8.0.1.5.1",
+    "version": "8.0.2.0.0",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "website": "https://simetri-sinergi.id",
     "license": "AGPL-3",
     "depends": [
         "accountant_app",
+        "base_workflow_policy",
+        "base_print_policy",
+        "base_multiple_approval",
+        "base_cancel_reason",
     ],
     "data": [
         "security/ir.model.access.csv",
+        "security/ir_module_category_data.xml",
+        "security/res_groups_data.xml",
+        "data/base_cancel_reason_configurator_data.xml",
         "data/ir_sequence_data.xml",
+        "data/base_workflow_policy_data.xml",
         "views/accountant_config_setting_views.xml",
         "views/accountant_partner_arrangement_views.xml",
     ],
