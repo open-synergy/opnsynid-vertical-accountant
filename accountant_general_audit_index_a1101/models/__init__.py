b6c5c784dabdcbc1919aa7282d5c06b71556c687
Nurazmi <azmimr67@gmail.com>
2021-07-23 16:16:50 +0700

    [8.0.1.0.0] accountant_general_audit_index_a1101

---
+++ accountant_general_audit_index_a1101/models/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    accountant_client_account_type_set,
+    accountant_client_account_type,
+    accountant_client_trial_balance,
+    accountant_client_trial_balance_detail,
+    accountant_client_trial_balance_standard_detail,
+    accountant_client_trial_balance_computation,
+    accountant_client_account_type_computation_item,
+    accountant_trial_balance_computation_item,
+    tier_definition,
+)
