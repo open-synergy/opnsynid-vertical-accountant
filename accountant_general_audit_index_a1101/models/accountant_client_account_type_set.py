b74f4d456035facb73bb0fa4c1797ae78085821b
Nurazmi <azmimr67@gmail.com>
2021-07-26 12:31:21 +0700

    [8.0.1.0.0] accountant_general_audit_index_a1101 Revisi workflow dan sequence pada accountant_type_set

--- accountant_general_audit_index_a1101/models/accountant_client_account_type_set.py
+++ accountant_general_audit_index_a1101/models/accountant_client_account_type_set.py
@@ -9,32 +9,8 @@ from openerp import fields, models
 class AccountantClientAccountTypeSet(models.Model):
     _inherit = "accountant.client_account_type_set"
 
-    # general Audit Index
-    accountant_general_audit_index_a1101_confirm_grp_ids = fields.Many2many(
-        string="Allow To Confirm Accountant General Audit Index",
-        comodel_name="res.groups",
-        relation="rel_accountant_type_set_confirm_general_audit_index_a1101",
-        column1="account_type_set_id",
-        column2="group_id",
-    )
-    accountant_general_audit_index_a1101_restart_validation_grp_ids = fields.Many2many(
-        string="Allow To Restart Validation Accountant General Audit Index",
-        comodel_name="res.groups",
-        relation="rel_acc_type_set_restart_validation_gen_audit_index_a1101",
-        column1="account_type_set_id",
-        column2="group_id",
-    )
-    accountant_general_audit_index_a1101_cancel_grp_ids = fields.Many2many(
-        string="Allow To Cancel Accountant General Audit Index",
-        comodel_name="res.groups",
-        relation="rel_accountant_type_set_cancel_general_audit_index_a1101",
-        column1="account_type_set_id",
-        column2="group_id",
-    )
-    accountant_general_audit_index_a1101_restart_grp_ids = fields.Many2many(
-        string="Allow To Restart Accountant General Audit Index",
-        comodel_name="res.groups",
-        relation="rel_accountant_type_set_restart_general_audit_index_a1101",
-        column1="account_type_set_id",
-        column2="group_id",
+    accountant_general_audit_index_a1101_sequence_id = fields.Many2one(
+        string="Accountant General Audit Index Sequence",
+        comodel_name="ir.sequence",
+        company_dependent=True,
     )
