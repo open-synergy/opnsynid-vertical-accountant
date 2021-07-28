7e3d5bc85f95acfd948ccca9f6e20e94a304615a
Nurazmi <azmimr67@gmail.com>
2021-07-28 17:57:15 +0700

    [8.0.1.0.0] accountant_general_audit_index_a1104
    Fix #90

---
+++ accountant_general_audit_index_a1104/models/accountant_general_audit.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 PT Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class AccountantGeneralAudit(models.Model):
+    _name = "accountant.general_audit"
+    _inherit = "accountant.general_audit"
+
+    @api.multi
+    @api.depends(
+        "index_a1104_ids",
+    )
+    def _compute_index_a1104_id(self):
+        for document in self:
+            document.index_a1104_id = False
+            if len(document.index_a1104_ids) > 0:
+                document.index_a1104_id = document.index_a1104_ids[0]
+
+    index_a1104_ids = fields.Many2many(
+        string="Trial Balance",
+        comodel_name="accountant.general_audit_index_a1104",
+        relation="rel_general_audit_2_index_a1104",
+        column1="general_audit_id",
+        column2="index_a1104_id",
+    )
+    index_a1104_id = fields.Many2one(
+        string="# Index A.110.4",
+        comodel_name="accountant.general_audit_index_a1104",
+        compute="_compute_index_a1104_id",
+        store=True,
+    )
