ffa5478dcea968e448ab627a26528b154b486b63
Nurazmi <azmimr67@gmail.com>
2021-07-28 17:57:15 +0700

    [8.0.1.0.0] accountant_general_audit_index_a1104
    Fix #90

---
+++ accountant_general_audit_index_a1104/models/accountant_general_audit_index_a1104_status.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class AccountantGeneralAuditIndexA1104Status(models.Model):
+    _name = "accountant.general_audit_index_a1104_status"
+    _description = "Index A.110.4 Status"
+    _order = "sequence, id"
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+        required=True,
+    )
+    sequence = fields.Integer(
+        string="Sequence",
+        required=True,
+        default=5,
+    )
+    note = fields.Text(
+        string="Note",
+        required=False,
+    )
