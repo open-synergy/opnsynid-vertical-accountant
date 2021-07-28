69f895253424f052640f37c52fee25c9d00bdbf6
Nurazmi <azmimr67@gmail.com>
2021-07-28 17:57:15 +0700

    [8.0.1.0.0] accountant_general_audit_index_a1104

---
+++ accountant_general_audit_index_a1104/models/accountant_general_audit_index_a1104.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
+
+
+class AccountantGeneralAuditIndexA1104(models.Model):
+    _name = "accountant.general_audit_index_a1104"
+    _description = "Accountant General Audit Index A1104"
+    _inherit = [
+        "mail.thread",
+        "tier.validation",
+        "base.sequence_document",
+        "base.workflow_policy_object",
+        "base.cancel.reason_common",
+    ]
+    _state_from = ["draft", "confirm"]
+    _state_to = ["valid"]
+
+    @api.multi
+    @api.depends(
+        "company_id",
+    )
+    def _compute_policy(self):
+        _super = super(AccountantGeneralAuditIndexA1104, self)
+        _super._compute_policy()
+
+    name = fields.Char(
+        string="# Document",
+        default="/",
+        required=True,
+        copy=False,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    general_audit_id = fields.Many2one(
+        string="General Audit",
+        comodel_name="accountant.general_audit",
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    account_type_set_id = fields.Many2one(
+        string="Accoount Type Set",
+        comodel_name="accountant.client_account_type_set",
+        related="general_audit_id.account_type_set_id",
+        readonly=True,
+        store=False,
+    )
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        readonly=True,
+        related="general_audit_id.company_id",
+    )
+    partner_id = fields.Many2one(
+        string="Partner",
+        comodel_name="res.partner",
+        related="general_audit_id.partner_id",
+        required=True,
+        readonly=True,
+    )
+    question_1 = fields.Selection(
+        string="Question 1",
+        selection=[
+            ("yes", "Yes"),
+            ("no", "No"),
+        ],
+        readonly=True,
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    question_2 = fields.Selection(
+        string="Question 2",
+        selection=[
+            ("yes", "Yes"),
+            ("no", "No"),
+        ],
+        readonly=True,
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    question_3a = fields.Selection(
+        string="Question 3a",
+        selection=[
+            ("yes", "Yes"),
+            ("no", "No"),
+        ],
+        readonly=True,
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    question_3b = fields.Selection(
+        string="Question 3b",
+        selection=[
+            ("yes", "Yes"),
+            ("no", "No"),
+        ],
+        readonly=True,
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    status_id = fields.Many2one(
+        string="Status",
+        comodel_name="accountant.general_audit_index_a1104_status",
+        required=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("valid", "Valid"),
+            ("cancel", "Cancelled"),
+        ],
+        copy=False,
+        default="draft",
+        required=True,
+        readonly=True,
+    )
+    confirm_ok = fields.Boolean(
+        string="Can Confirm",
+        compute="_compute_policy",
+    )
+    restart_validation_ok = fields.Boolean(
+        string="Can Restart Validation",
+        compute="_compute_policy",
+    )
+    valid_ok = fields.Boolean(
+        string="Can Valid",
+        compute="_compute_policy",
+    )
+    cancel_ok = fields.Boolean(
+        string="Can Cancel",
+        compute="_compute_policy",
+    )
+    restart_ok = fields.Boolean(
+        string="Can Restart",
+        compute="_compute_policy",
+    )
+    confirm_date = fields.Datetime(
+        string="Confirmation Date",
+        readonly=True,
+        copy=False,
+    )
+    confirm_user_id = fields.Many2one(
+        string="Confirmed By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    valid_date = fields.Datetime(
+        string="Validation Date",
+        readonly=True,
+        copy=False,
+    )
+    valid_user_id = fields.Many2one(
+        string="Valid By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    cancel_date = fields.Datetime(
+        string="Cancel Date",
+        readonly=True,
+        copy=False,
+    )
+    cancel_user_id = fields.Many2one(
+        string="Cancelled By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+
+    @api.multi
+    def action_confirm(self):
+        for record in self:
+            record.write(record._prepare_confirm_data())
+            record.request_validation()
+
+    @api.multi
+    def action_valid(self):
+        for record in self:
+            record.write(record._prepare_valid_data())
+
+    @api.multi
+    def action_cancel(self):
+        for record in self:
+            record.write(record._prepare_cancel_data())
+
+    @api.multi
+    def action_restart(self):
+        for record in self:
+            record.write(record._prepare_restart_data())
+
+    @api.multi
+    def _prepare_confirm_data(self):
+        self.ensure_one()
+        return {
+            "state": "confirm",
+            "confirm_date": fields.Datetime.now(),
+            "confirm_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_valid_data(self):
+        self.ensure_one()
+        sequence = self._create_sequence()
+        return {
+            "state": "valid",
+            "name": sequence,
+            "valid_date": fields.Datetime.now(),
+            "valid_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_cancel_data(self):
+        self.ensure_one()
+        return {
+            "state": "cancel",
+            "cancel_date": fields.Datetime.now(),
+            "cancel_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_restart_data(self):
+        self.ensure_one()
+        return {
+            "state": "draft",
+            "confirm_date": False,
+            "confirm_user_id": False,
+            "valid_date": False,
+            "valid_user_id": False,
+            "cancel_date": False,
+            "cancel_user_id": False,
+        }
+
+    @api.multi
+    def unlink(self):
+        strWarning = _("You can only delete data on draft state")
+        for record in self:
+            if record.state != "draft":
+                if not self.env.context.get("force_unlink", False):
+                    raise UserError(strWarning)
+        _super = super(AccountantGeneralAuditIndexA1104, self)
+        _super.unlink()
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.name == "/":
+                name = "*" + str(record.id)
+            else:
+                name = record.name
+            result.append((record.id, name))
+        return result
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(AccountantGeneralAuditIndexA1104, self)
+        _super.validate_tier()
+        for record in self:
+            if record.validated:
+                record.action_valid()
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(AccountantGeneralAuditIndexA1104, self)
+        _super.restart_validation()
+        for record in self:
+            record.request_validation()
