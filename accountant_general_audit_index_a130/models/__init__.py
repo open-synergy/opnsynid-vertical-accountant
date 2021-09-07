2e41a52e93011284e08c5086ff2889f0223f29d1
Nurazmi <azmimr67@gmail.com>
2021-09-07 14:45:35 +0700

    [8.0.1.0.0] accountant_general_audit_index_a130

---
+++ accountant_general_audit_index_a130/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    accountant_client_account_type_set,
+    accountant_general_audit_index_a130_status,
+    accountant_general_audit_index_a130,
+    accountant_general_audit,
+    tier_definition,
+)
---
+++ accountant_general_audit_index_a130/models/accountant_client_account_type_set.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class AccountantClientAccountTypeSet(models.Model):
+    _name = "accountant.client_account_type_set"
+    _inherit = "accountant.client_account_type_set"
+
+    index_a130_sequence_id = fields.Many2one(
+        string="Index A.130 Sequence",
+        comodel_name="ir.sequence",
+        company_dependent=True,
+    )
