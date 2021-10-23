5859d8c803db73640a6df3b1ab5b98b94787c426
Andhitia Rama <andhitia.r@gmail.com>
2021-10-23 21:55:26 +0700

    [accountant_general_audit] 8.0.3.7.0
    * fix menus order and label
    * Remode valid_ok, valid_user_id, and valid_date from (1) general audit, (2) TB, and (3) adjustment entry
    * Add groups for adjustment entry usability

--- accountant_general_audit/__openerp__.py
+++ accountant_general_audit/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Accountant General Audit",
-    "version": "8.0.3.6.1",
+    "version": "8.0.3.7.0",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "website": "https://simetri-sinergi.id",
     "license": "AGPL-3",
