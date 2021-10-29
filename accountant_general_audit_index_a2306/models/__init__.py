71f90e42bf0c43a28bbfd787b0d6d78f9fc0abb2
Nurazmi <azmimr67@gmail.com>
2021-10-29 21:16:48 +0700

    [8.0.1.0.0] accountant_general_audit_index_a2306 Task T003779

---
+++ accountant_general_audit_index_a2306/models/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    accountant_client_account_type_set,
+    accountant_general_audit_index_a2306_status,
+    accountant_general_audit_index_a2306,
+    accountant_general_audit,
+    accountant_going_concern_category,
+    accountant_going_concern,
+    accountant_general_audit_going_concern,
+    tier_definition,
+)
