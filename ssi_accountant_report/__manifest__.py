08afa4587d9b78e94639b1ca049392a4b7754b55
Asrulbastian <asrulbastianyunas@gmail.com>
2022-04-06 23:32:50 +0700

    [ssi_accountant_report] 14.0.1.0.0

--- ssi_accountant_report/__manifest__.py
+++ ssi_accountant_report/__manifest__.py
@@ -1,11 +1,6 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
-# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Manage Accountant Report",
     "version": "14.0.1.0.0",
-    "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -13,14 +8,18 @@
     "depends": [
         "mail",
         "ssi_accountant",
-        "ssi_policy_mixin",
-        "ssi_sequence_mixin",
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
     ],
     "data": [
         "security/ir.model.access.csv",
-        "security/ir_module_category_data.xml",
-        "security/res_groups_data.xml",
         "data/ir_sequence_data.xml",
+        "data/sequence_template_accountant_report_data.xml",
+        "data/policy_template_accountant_report_data.xml",
+        "data/approval_template_accountant_report_data.xml",
         "views/accountant_report_opinion_views.xml",
         "views/accountant_report_method_views.xml",
         "views/accountant_service_views.xml",
