5c649ee90469263668741efc8ee810032eebea9c
Andhitia Rama <andhitia.r@gmail.com>
2022-11-15 13:36:11 +0700

    [ssi_accountant_general_audit_ws_ra130] 14.0.1.0.1

--- ssi_accountant_general_audit_ws_ra130/models/accountant_general_audit_ws_ra1301_materiality_mapping.py
+++ ssi_accountant_general_audit_ws_ra130/models/accountant_general_audit_ws_ra1301_materiality_mapping.py
@@ -1,7 +1,7 @@
 # Copyright 2021 PT Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class AccountantGeneralAuditWS1301MaterialityMapping(models.Model):
