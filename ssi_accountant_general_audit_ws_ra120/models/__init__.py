d3694bcdd6510ed2a58e0d999dc36d2fecf6548c
Andhitia Rama <andhitia.r@gmail.com>
2022-11-23 16:59:15 +0700

    [ssi_accountant_general_audit_ws_ra120] 14.0.1.0.0

---
+++ ssi_accountant_general_audit_ws_ra120/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from . import (
+    ws_ra1201,
+    ws_ra1201_account_mapping,
+)
