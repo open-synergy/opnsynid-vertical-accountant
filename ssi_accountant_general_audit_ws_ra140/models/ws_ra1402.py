b675ed2e8291fea86e42850bf21b625287ddf6dc
Andhitia Rama <andhitia.r@gmail.com>
2022-12-02 00:20:08 +0700

    [ssi_accountant_general_audit_ws_ra140] 14.0.1.4.0

--- ssi_accountant_general_audit_ws_ra140/models/ws_ra1402.py
+++ ssi_accountant_general_audit_ws_ra140/models/ws_ra1402.py
@@ -4,6 +4,8 @@
 
 from odoo import api, fields, models
 
+from odoo.addons.ssi_decorator import ssi_decorator
+
 
 class WSAuditRA1402(models.Model):
     _name = "ws_ra1402"
@@ -23,7 +25,7 @@ class WSAuditRA1402(models.Model):
         comodel_name="ws_ra1402.ratio",
         inverse_name="worksheet_id",
         domain=[
-            ("computation_item_id.category", "=", "liquidity"),
+            ("financial_ratio_id.category", "=", "liquidity"),
         ],
     )
     activity_ratio_ids = fields.One2many(
@@ -31,7 +33,7 @@ class WSAuditRA1402(models.Model):
         comodel_name="ws_ra1402.ratio",
         inverse_name="worksheet_id",
         domain=[
-            ("computation_item_id.category", "=", "activity"),
+            ("financial_ratio_id.category", "=", "activity"),
         ],
     )
     solvency_ratio_ids = fields.One2many(
@@ -39,7 +41,7 @@ class WSAuditRA1402(models.Model):
         comodel_name="ws_ra1402.ratio",
         inverse_name="worksheet_id",
         domain=[
-            ("computation_item_id.category", "=", "solvency"),
+            ("financial_ratio_id.category", "=", "solvency"),
         ],
     )
     profitability_ratio_ids = fields.One2many(
@@ -47,23 +49,84 @@ class WSAuditRA1402(models.Model):
         comodel_name="ws_ra1402.ratio",
         inverse_name="worksheet_id",
         domain=[
-            ("computation_item_id.category", "=", "profitability"),
+            ("financial_ratio_id.category", "=", "profitability"),
         ],
     )
 
     @api.onchange("general_audit_id")
     def onchange_ratio_ids(self):
         self.update({"ratio_ids": [(5, 0, 0)]})
+        FinancialRatio = self.env["accountant.financial_ratio"]
         if self.general_audit_id:
             result = []
-            for computation in self.general_audit_id.computation_ids:
+            for ratio in FinancialRatio.search([]):
                 result.append(
                     (
                         0,
                         0,
                         {
-                            "general_audit_computation_item_id": computation.id,
+                            "financial_ratio_id": ratio.id,
                         },
                     )
                 )
             self.update({"ratio_ids": result})
+
+    def action_compute_ratio(self):
+        for record in self:
+            record._recompute_computation()
+
+    @ssi_decorator.post_confirm_action()
+    def _recompute_computation(self):
+        self.ensure_one()
+        additional_dict = self._get_additional_dict()
+        for computation in self.ratio_ids:
+            additionaldict = computation._recompute(additional_dict)
+            additional_dict = additionaldict
+
+        self.general_audit_id._recompute_extrapolation_computation()
+
+    def _get_additional_dict(self):
+        self.ensure_one()
+        result = {"account_type": {}, "account_group": {}, "computation": {}}
+        # Load general audit's standard detail to dict
+        for data in self.general_audit_id.standard_detail_ids:
+            result["account_type"].update(
+                {
+                    data.type_id.code: {
+                        "extrapolation": data.extrapolation_balance,
+                        "interim": data.interim_balance,
+                        "previous": data.previous_balance,
+                        "extrapolation_avg": data.extrapolation_avg,
+                        "interim_avg": data.interim_avg,
+                    },
+                }
+            )
+
+        # Load general audit's group summary to dict
+        for data in self.general_audit_id.group_detail_ids:
+            result["account_group"].update(
+                {
+                    data.group_id.code: {
+                        "extrapolation": data.extrapolation_balance,
+                        "interim": data.interim_balance,
+                        "previous": data.previous_balance,
+                        "extrapolation_avg": data.extrapolation_avg,
+                        "interim_avg": data.interim_avg,
+                    },
+                }
+            )
+
+        # Load general audit's computation to dict
+        for data in self.general_audit_id.computation_ids:
+            result["computation"].update(
+                {
+                    data.computation_item_id.code: {
+                        "extrapolation": data.extrapolation_amount,
+                        "interim": data.interim_amount,
+                        "previous": data.previous_amount,
+                        "extrapolation_avg": data.extrapolation_avg_amount,
+                        "interim_avg": data.interim_avg_amount,
+                    },
+                }
+            )
+        return result
--- ssi_accountant_general_audit_ws_ra140/models/ws_ra1402_ratio.py
+++ ssi_accountant_general_audit_ws_ra140/models/ws_ra1402_ratio.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
 
 from odoo import fields, models
+from odoo.tools.safe_eval import safe_eval as eval  # pylint: disable=redefined-builtin
 
 
 class WSAuditRA1402Ratio(models.Model):
@@ -15,30 +16,28 @@ class WSAuditRA1402Ratio(models.Model):
         required=True,
         ondelete="cascade",
     )
-    general_audit_computation_item_id = fields.Many2one(
-        string="General Audit Computation Item",
-        comodel_name="accountant.general_audit_computation",
+    financial_ratio_id = fields.Many2one(
+        string="Ratio",
+        comodel_name="accountant.financial_ratio",
         required=True,
     )
-    computation_item_id = fields.Many2one(
-        string="Computation Item",
-        comodel_name="accountant.trial_balance_computation_item",
-        related="general_audit_computation_item_id.computation_item_id",
-        store=True,
+    category = fields.Selection(
+        string="Category",
+        related="financial_ratio_id.category",
     )
     extrapolation_amount = fields.Float(
         string="Extrapolation Amount",
-        related="general_audit_computation_item_id.extrapolation_amount",
+        related=False,
         store=True,
     )
     interim_amount = fields.Float(
         string="Interim Amount",
-        related="general_audit_computation_item_id.interim_amount",
+        related=False,
         store=True,
     )
     previous_amount = fields.Float(
         string="Previous Amount",
-        related="general_audit_computation_item_id.previous_amount",
+        related=False,
         store=True,
     )
     industry_average = fields.Float(
@@ -47,3 +46,46 @@ class WSAuditRA1402Ratio(models.Model):
     analysis = fields.Char(
         string="Analysis",
     )
+
+    def _get_localdict(self):
+        self.ensure_one()
+        return {
+            "env": self.env,
+            "document": self,
+        }
+
+    def _recompute(self, additional_dict):
+        self.ensure_one()
+        python_code = self.financial_ratio_id.python_code
+
+        localdict = self._get_localdict()
+        localdict.update(additional_dict)
+        try:
+            eval(
+                python_code,
+                localdict,
+                mode="exec",
+                nocopy=True,
+            )
+            extrapolation_amount = localdict["result_extrapolation"]
+            interim_amount = localdict["result_interim"]
+            previous_amount = localdict["result_previous"]
+            additional_dict.update(
+                {
+                    self.financial_ratio_id.code: {
+                        "extrapolation": extrapolation_amount,
+                        "interim": interim_amount,
+                        "previous": previous_amount,
+                    }
+                }
+            )
+        except Exception:
+            extrapolation_amount = interim_amount = previous_amount = 0.0
+        self.write(
+            {
+                "extrapolation_amount": extrapolation_amount,
+                "interim_amount": interim_amount,
+                "previous_amount": previous_amount,
+            }
+        )
+        return additional_dict
