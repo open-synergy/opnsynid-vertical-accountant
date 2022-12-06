c2d33c89472f54d0e7c103ccd3d22cef196bb20c
Andhitia Rama <andhitia.r@gmail.com>
2022-12-06 19:41:31 +0700

    [ssi_accountant_general_audit_ws_ra220] 14.0.1.0.0

---
+++ ssi_accountant_general_audit_ws_ra220/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from . import (
+    ws_ra220,
+    ws_ra2201,
+    ws_ra22011,
+    ws_ra22012,
+    ws_ra2202,
+    ws_ra22021,
+    ws_ra2203,
+)
