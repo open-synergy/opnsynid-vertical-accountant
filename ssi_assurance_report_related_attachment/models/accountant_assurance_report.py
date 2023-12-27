c184508db759bd0972b82e857dcd97e115544669
Andhitia Rama <andhitia.r@gmail.com>
2023-12-27 23:44:11 +0700

    [UPD] ssi_assurance_report_related_attachment

--- ssi_assurance_report_related_attachment/models/accountant_assurance_report.py
+++ ssi_assurance_report_related_attachment/models/accountant_assurance_report.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountantAssuranceReport(models.Model):
@@ -12,3 +12,8 @@ class AccountantAssuranceReport(models.Model):
         "mixin.related_attachment",
     ]
     _related_attachment_create_page = True
+
+    @api.onchange("service_id")
+    def onchange_related_attachment_template_id(self):
+        self.related_attachment_template_id = self._get_template_related_attachment()
+        self.onchange_related_attachment_ids()
