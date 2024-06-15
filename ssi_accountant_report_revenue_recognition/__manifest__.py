2bcc8807c12da59ebbec86ff690768f2093445ab
Miftahussalam <miftah_salam@ymail.com>
2024-06-15 17:07:33 +0700

    [ADD] ssi_accountant_report_revenue_recognition

---
+++ ssi_accountant_report_revenue_recognition/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Accountant Report + Revenue Recognition Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_accountant_report",
+        "ssi_revenue_recognition",
+    ],
+    "data": [
+        "views/accountant_report_mixin_views.xml",
+    ],
+    "auto_install": True,
+}
